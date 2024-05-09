bf891e07b2a0c4edad39921f0e74f234923303b0
Kipkoech78 <lkipkoech@kabarak.ac.ke>
2024-05-09 22:10:45 +0300

    update keeper v2

---
+++ 77b57718-1e07-41c7-aa97-0a0e932361ad/src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+import App from "./App";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>{props.title} </h1>
+      <p>{props.content}</p>
+    </div>
+  );
+}
+
+export default Note;
